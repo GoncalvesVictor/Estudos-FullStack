17eb0cc890e404a3eb3ddbc2c122b91f77de0711
GoncalvesVictor <gonca94v@gmail.com>
2023-03-16 18:24:35 -0300

    Criado e configurado projeto e BD. Criado mapeamento das classes.

---
+++ 02 - Projeto CRM - Java Spring Boot + Angular 11/crm-virtual/src/main/java/com/victor/crmvirtual/CrmVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.victor.crmvirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmVirtualApplication.class, args);
+	}
+
+}
---
+++ 02 - Projeto CRM - Java Spring Boot + Angular 11/crm-virtual/src/test/java/com/victor/crmvirtual/CrmVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.crmvirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
